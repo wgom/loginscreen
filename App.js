cc7f02c877e667e9e4d7485e8699d19fbc9b94d5
willians ojeda <willians_ojeda@hacienda.gov.py>
2024-08-20 10:49:04 -0400

    tutoria finalizado

--- App.js
+++ App.js
@@ -8,7 +8,10 @@ const profilePicture = 'https://randomuser.me/api/portraits/men/34.jpg';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{uri}} style={[styles.image, StyleSheet.absoluteFill]}></Image>
+      <Image source={{uri}} style={[styles.image, StyleSheet.absoluteFill]}/>
+      <View style={{width: 100, height: 100, backgroundColor: 'blue', top: 120, position: 'absolute', transform: [{rotate: '25deg'}] }}></View>
+      <View style={{width: 100, height: 100, backgroundColor: 'purple', position: 'absolute'}}></View>
+      <View style={{width: 100, height: 100, backgroundColor: 'red', bottom: 120, position: 'absolute', borderRadius: 50, transform: [{rotate: '50deg'}] }}></View>
       <ScrollView contentContainerStyle={{
         flex: 1,
         width: '100%',
@@ -18,15 +21,21 @@ export default function App() {
       }}>
         <BlurView intensity={100}>
           <View style={styles.login}>
-            <Image source={{uri: profilePicture}} style={styles.profilePicture}></Image>
+            <Image source={{uri: profilePicture}} style={styles.profilePicture}/>
             <View>
               <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>E-mail</Text>
-              <TextInput style={styles.input} placeholder="tucorreo@dominio.com"></TextInput>
+              <TextInput style={styles.input} placeholder="tucorreo@dominio.com"/>
             </View>
             <View>
               <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
-              <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}></TextInput>
+              <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}/>
             </View>
+            <TouchableOpacity style={[styles.button, {backgroundColor: '#00CFEB90'}]}>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, {backgroundColor: '#6792F090'}]}>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Create Account</Text>
+            </TouchableOpacity>
           </View>
         </BlurView>
       </ScrollView>
@@ -54,6 +63,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 10,
     alignItems: 'center',
+    //backgroundColor: "rgba(255, 255, 255, 0.9)"
   },
   profilePicture: {
     width: 100,
@@ -74,4 +84,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff90',
     marginBottom: 20,
   },
+  button: {
+    width: 250,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    borderColor: '#fff',
+    borderWidth: 2,
+  }
 });
