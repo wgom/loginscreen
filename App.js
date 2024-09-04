ffb6387a90b65873695878889d695b3bb3aa8dff
willians ojeda <willians_ojeda@hacienda.gov.py>
2024-09-04 10:09:51 -0400

    commit de limpieza

--- App.js
+++ App.js
@@ -1,40 +1,118 @@
-import react from 'react';
-import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button } from 'react-native';
-import { BlurView } from 'expo-blur';
+import React from "react";
+import {
+  Image,
+  Text,
+  StyleSheet,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Button,
+} from "react-native";
+import { BlurView } from "expo-blur";
 
-const uri = 'https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg';
-const profilePicture = 'https://randomuser.me/api/portraits/men/34.jpg';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase-config";
 
-export default function App() {
+const uri = "https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg";
+const profilePicture = "https://randomuser.me/api/portraits/men/34.jpg";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+function LoginScreen() {
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Image source={{uri}} style={[styles.image, StyleSheet.absoluteFill]}/>
-      <View style={{width: 100, height: 100, backgroundColor: 'blue', top: 120, position: 'absolute', transform: [{rotate: '25deg'}] }}></View>
-      <View style={{width: 100, height: 100, backgroundColor: 'purple', position: 'absolute'}}></View>
-      <View style={{width: 100, height: 100, backgroundColor: 'red', bottom: 120, position: 'absolute', borderRadius: 50, transform: [{rotate: '50deg'}] }}></View>
-      <ScrollView contentContainerStyle={{
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
+      <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "blue",
+          top: 120,
+          position: "absolute",
+          transform: [{ rotate: "25deg" }],
+        }}
+      ></View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "purple",
+          position: "absolute",
+        }}
+      ></View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "red",
+          bottom: 120,
+          position: "absolute",
+          borderRadius: 50,
+          transform: [{ rotate: "50deg" }],
+        }}
+      ></View>
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <BlurView intensity={100}>
           <View style={styles.login}>
-            <Image source={{uri: profilePicture}} style={styles.profilePicture}/>
+            <Image
+              source={{ uri: profilePicture }}
+              style={styles.profilePicture}
+            />
             <View>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>E-mail</Text>
-              <TextInput style={styles.input} placeholder="tucorreo@dominio.com"/>
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                E-mail
+              </Text>
+              <TextInput
+                style={styles.input}
+                placeholder="tucorreo@dominio.com"
+              />
             </View>
             <View>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
-              <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}/>
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Password
+              </Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry={true}
+              />
             </View>
-            <TouchableOpacity style={[styles.button, {backgroundColor: '#00CFEB90'}]}>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text>
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#00CFEB90" }]}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Login
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, {backgroundColor: '#6792F090'}]}>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Create Account</Text>
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#6792F090" }]}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Create Account
+              </Text>
             </TouchableOpacity>
           </View>
         </BlurView>
@@ -43,55 +121,59 @@ export default function App() {
   );
 }
 
+export default function App() {
+  return <LoginScreen />;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
   login: {
     width: 350,
     height: 500,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderWidth: 2,
     borderRadius: 10,
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center",
     //backgroundColor: "rgba(255, 255, 255, 0.9)"
   },
   profilePicture: {
     width: 100,
     height: 100,
     borderRadius: 50,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderWidth: 2,
     marginVertical: 30,
   },
   input: {
     width: 250,
     height: 40,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderWidth: 2,
     borderRadius: 10,
     padding: 10,
     marginVertical: 10,
-    backgroundColor: '#ffffff90',
+    backgroundColor: "#ffffff90",
     marginBottom: 20,
   },
   button: {
     width: 250,
     height: 40,
     borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginVertical: 10,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderWidth: 2,
-  }
+  },
 });
---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBB8_pSgkZtwlHI6dbP4ME8NyI4QJgokIo",
+  authDomain: "blur-login-1e242.firebaseapp.com",
+  projectId: "blur-login-1e242",
+  storageBucket: "blur-login-1e242.appspot.com",
+  messagingSenderId: "74905155775",
+  appId: "1:74905155775:web:249fcaec308dc650d12e0a"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
